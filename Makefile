71d978696dd6b928ff23228b5fa43c9cf3e74c38
Bartłomiej Rapacz <tsiruu@gmail.com>
2023-02-09 15:49:02 +0100

    Added artists check using custom runtime mappings

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
-install:
+setup:
 	pip3 install -r requirements.txt
 	docker-compose up -d
+	@echo "It may take a moment for elasticsearch to become responsive!"
 
 test:
 	python3 -m unittest *_test.py
--- first_test.py
+++ first_test.py
@@ -11,6 +11,8 @@ class ElasticMixin:
         pass
 
 class TestElasticQueries(ElasticMixin, unittest.TestCase):
+    def extract_song_ids(self, result):
+        return [hit['_id'] for hit in result['hits']['hits']]
 
     def setUp(self):
         super().setUp()
@@ -98,8 +100,8 @@ class TestElasticQueries(ElasticMixin, unittest.TestCase):
                 }
             }
         )
-        hits = [hit['_id'] for hit in result['hits']['hits']]
-        self.assertEqual(hits, ['numb', 'numb-encore'])
+        hits = self.extract_song_ids(result)
+        self.assertEqual(['numb', 'numb-encore'], hits)
 
     def test_exclude_songs_when_none_of_the_artists_have_id(self):
         result = self.client.search(
@@ -115,8 +117,8 @@ class TestElasticQueries(ElasticMixin, unittest.TestCase):
                 }
             }
         )
-        hits = [hit['_id'] for hit in result['hits']['hits']]
-        self.assertEqual(hits, ['numb', 'numb-encore', 'lying-from-you'])
+        hits = self.extract_song_ids(result)
+        self.assertEqual(['numb', 'numb-encore', 'lying-from-you'], hits)
 
     def test_exclude_songs_when_any_artists_does_not_have_id(self):
         result = self.client.search(
@@ -124,12 +126,52 @@ class TestElasticQueries(ElasticMixin, unittest.TestCase):
             body={
                 'query': {
                     'bool': {
+                        'must': [
+                            {'range': { 'artists_count': { 'gt': 0 } }},
+                            {'match': { 'artists_without_id_count': 0 }}
+                        ]
                     }
-                }
+                },
+                'runtime_mappings': self.get_runtime_mappings(),
             }
         )
-        hits = [hit['_id'] for hit in result['hits']['hits']]
-        self.assertEqual(hits, ['numb', 'lying-from-you'])
+        hits = self.extract_song_ids(result)
+        self.assertEqual(['numb', 'lying-from-you'], hits)
+
+    def get_runtime_mappings(self):
+        artists_with_id_count_script = """
+            int count = 0;
+            for(def artist : params['_source']['artists']) {
+                if(null != artist['id'] || '' != artist['id']) {
+                    count++;
+                }
+            }
+            emit(count);
+        """
+
+        # same but with different operator
+        artists_without_id_count_script = artists_with_id_count_script.replace(' != ', ' == ')
+
+        return {
+            'artists_with_id_count': {
+                'type': 'long',
+                'script': {
+                    'source': artists_with_id_count_script
+                }
+            },
+            'artists_without_id_count': {
+                'type': 'long',
+                'script': {
+                    'source': artists_without_id_count_script
+                }
+            },
+            'artists_count': {
+                'type': 'long',
+                'script': {
+                    'source': """emit(params['_source']['artists'].size())"""
+                }
+            }
+        }
 
 
 
